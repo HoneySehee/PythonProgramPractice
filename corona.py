e094babe29c82cf99609f6d153287f1cc74f4349
ParkJongMin <46151375+HoneySehee@users.noreply.github.com>
2020-05-19 00:17:36 +0900

    Add files via upload

---
+++ corona.py
@@ -0,0 +1,28 @@
+from urllib.parse import quote_plus
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
+
+url = "https://hazard.yahoo.co.jp/article/covid19tokyo"
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+html = driver.page_source
+soup = BeautifulSoup(html, "lxml")
+
+dashBoard = soup.select('.dashBoard')
+
+for i in dashBoard:
+    kosin = (i.select_one('.notes__item.notes__item--right').text)
+    jokyo = (i.select_one('.dashBoard__main').text)
+
+driver.close()
+
+url = 'https://notify-api.line.me/api/notify'
+access_token = '8menQ9ILfMC7HK8a2MSN8fJCsX9AJETyJj38ryVs6iS'
+headers = {'Authorization': 'Bearer ' + access_token}
+
+message = kosin, jokyo
+data = {'message': message}
+r = requests.post(url, headers=headers, params=data,)
