b6745222fba78709fb6ac8774e7f3713d40048f2
ParkJongMin <46151375+HoneySehee@users.noreply.github.com>
2020-08-13 23:49:46 +0900

    Add files via upload

---
+++ kisa_crawling.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+
+link = "https://www.krcert.or.kr"
+r = requests.get("https://www.krcert.or.kr/data/secNoticeList.do")
+bs = BeautifulSoup(r.text, "lxml")
+
+trs = bs.select("table.basicList.default > tbody > tr")
+
+for tr in trs:
+    tds = tr.select("td")
+    title = tds[1].select("a")[0].text
+    date = tds[4].text
+    links = tds[1].select("a")[0]['href']
+
+    url = 'https://notify-api.line.me/api/notify'
+    access_token = '8menQ9ILfMC7HK8a2MSN8fJCsX9AJETyJj38ryVs6iS'
+    headers = {'Authorization': 'Bearer ' + access_token}
+
+    message = title + '\n' + date + '\n' + '{}'.format(link) + links
+    data = {'message': message}
+    r = requests.post(url, headers=headers, params=data,)
