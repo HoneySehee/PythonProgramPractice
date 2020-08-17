cf575e907a1bba53db7d469f849bf0d861eb6728
ParkJongMin <46151375+HoneySehee@users.noreply.github.com>
2020-08-17 21:55:49 +0900

    Update folderlogging.py

--- folderlogging.py
+++ folderlogging.py
@@ -4,36 +4,39 @@ import os
 import time
 import requests
 
+def send_line(message):
+    url = 'https://notify-api.line.me/api/notify'
+    access_token = '8menQ9ILfMC7HK8a2MSN8fJCsX9AJETyJj38ryVs6iS'
+    headers = {'Authorization': 'Bearer ' + access_token}
+    message = message
+    payload = {'message': message}
+    r = requests.post(url, headers=headers, params=payload,)
+
 target_dir = 'D:\\text'
+
+print("감시중..")
 
 class FileChangeHandler(FileSystemEventHandler):
      def on_created(self, event):
-         filepath = event.src_path
-         filename = os.path.basename(filepath)
-
-         #Line API
-         url = 'https://notify-api.line.me/api/notify'
-         access_token = '라인토큰'
-         headers = {'Authorization': 'Bearer ' + access_token}
-
-         message = filepath + '\n'+'\n' + 'Create!!!'
-         payload = {'message': message}
-         r = requests.post(url, headers=headers, params=payload,)
+        filepath = event.src_path
+        filename = os.path.basename(filepath)
+         
+        send_line(filepath + '\n'+'\n' + 'Create!!!')
 
      def on_modified(self, event):
          filepath = event.src_path
          filename = os.path.basename(filepath)
-         print('%s changed' % filename)
+         print('{} changed'.format(filename))
 
      def on_deleted(self, event):
          filepath = event.src_path
          filename = os.path.basename(filepath)
-         print('%s deleted' % filename)
+         print('{} deleted'.format(filename))
 
      def on_moved(self, event):
          filepath = event.src_path
          filename = os.path.basename(filepath)
-         print('%s moved' % filename)
+         print('{} moved'.format(filename))
 
 if __name__ == "__main__":
      event_handler = FileChangeHandler()
