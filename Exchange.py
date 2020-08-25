333876ae11c803ce1817271f32d906a04a218221
ParkJongMin <46151375+HoneySehee@users.noreply.github.com>
2020-08-25 23:14:41 +0900

    Update Exchange.py

--- Exchange.py
+++ Exchange.py
@@ -3,36 +3,36 @@ import requests,time
 
 def send_line(*args):
     url = 'https://notify-api.line.me/api/notify'
-    access_token = 'MYTOKEN'
+    access_token = 'mkTxlIZhb4KbMU0isJlcrzTUUy8fLW5CCJhMG1IzwIA'
     headers = {'Authorization': 'Bearer ' + access_token}
     message = args
     payload = {'message': message}
     r = requests.post(url, headers=headers, params=payload,)
 
-
-url = "https://finance.naver.com/marketindex/exchangeList.nhn"
-r = requests.get(url)
-bs = BeautifulSoup(r.text, "lxml")
-
-
-trs = bs.select("table.tbl_exchange > tbody > tr")
-
 exchange = {}
 dictlist = []
 dictlists = []
 
-for tr in trs:
-    tds = tr.select("td")
-    name = tds[0].text.strip()
-    money = tds[1].text.strip()
-    exchange[name] = money
-
-for key,value in dict.items(exchange):
-    temp = [key,value]
-    dictlist.append(temp)
-
 while True:
-    time.sleep(5)
+    url = "https://finance.naver.com/marketindex/exchangeList.nhn"
+    r = requests.get(url)
+    bs = BeautifulSoup(r.text, "lxml")
+
+    trs = bs.select("table.tbl_exchange > tbody > tr")
+
+    for tr in trs:
+        tds = tr.select("td")
+        name = tds[0].text.strip()
+        money = tds[1].text.strip()
+        exchange[name] = money
+
+    for key,value in dict.items(exchange):
+        temp = [key,value]
+        dictlist.append(temp)
+    
     if dictlist[2] not in dictlists:
         send_line(dictlist[2],"원")
         dictlists.append(dictlist[2])
+
+    time.sleep(60*5)
+    print(dictlists)
