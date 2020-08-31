0d6d24755a7cc2f522a8e406382e7d310ac04b99
ParkJongMin <46151375+HoneySehee@users.noreply.github.com>
2020-08-31 22:19:26 +0900

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,16 +1,18 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from datetime import datetime
 import requests , time
 
 def send_line(*args):
     url = 'https://notify-api.line.me/api/notify'
-    access_token = 'TOKEN'
+    access_token = 'MyToken'
     headers = {'Authorization': 'Bearer ' + access_token}
     message = args
     payload = {'message': message}
     r = requests.post(url, headers=headers, params=payload,)
 
 send_lists = []
+today = datetime.today().strftime("%Y/%m/%d")
 
 while True:
     url = "https://hazard.yahoo.co.jp/article/covid19tokyo"
@@ -20,18 +22,17 @@ while True:
     html = driver.page_source
     soup = BeautifulSoup(html, "lxml")
 
-    dashBoard = soup.select('.dashBoard')
+    dashBoard = soup.select('div.dashBoard > dl.dashBoard__main > div')
+
+    corona = []
 
     for i in dashBoard:
-        item = (i.select('.notes__item')[1].text)
-        title = (i.select_one('.dashBoard__title').text)
-        main = (i.select('.dashBoard__main')[0].text)
-        
-        driver.close()
-        print(item)
-        
-        if item not in send_lists:
-            send_line(item + '\n', title + '\n', main + '\n')
-            send_lists.append(item)
+        corona.append(i)
 
-    time.sleep(3600)
+    if corona[1].text not in send_lists:
+        send_line("{} \n {}".format(today,corona[1].text))
+        send_lists.append(corona[1].text)
+    
+    print(send_lists)
+    driver.close()
+    time.sleep(60 * 30)
